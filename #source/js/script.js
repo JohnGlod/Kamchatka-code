ce299dcb347b7c909b8fb04056a99d5b30e4d83f
JohnGlod <EvgeniiDolgopolov@outlook.com>
2021-08-26 18:32:27 +0500

    mobile first  development

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,1 +1,12 @@
-'use strict'
+'use strict';
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+if (iconMenu) {
+  iconMenu.addEventListener('click', (e) => {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+}
