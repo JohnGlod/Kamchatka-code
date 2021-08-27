a661920ceab7ba6092cfdb71dfc7c1243ac68285
JohnGlod <EvgeniiDolgopolov@outlook.com>
2021-08-27 21:06:22 +0500

    first screen

--- #source/js/script.js
+++ #source/js/script.js
@@ -3,6 +3,9 @@
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
 
+const firstSlider = document.querySelector('.first-slider');
+
+
 if (iconMenu) {
   iconMenu.addEventListener('click', (e) => {
     document.body.classList.toggle('_lock');
@@ -10,3 +13,17 @@ if (iconMenu) {
     menuBody.classList.toggle('_active');
   });
 }
+
+let Main = new Swiper( firstSlider, 
+  {
+	centeredSlides: false,
+	loop: true, 
+	autoHeight: false,
+	slidesPerView: 'auto',
+	grabCursor: true,
+	spaceBetween: 35,
+	navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+});
