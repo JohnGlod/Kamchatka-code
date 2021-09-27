64302d942c7989a497d8488890625ed33aa77fe4
JohnGlod <EvgeniiDolgopolov@outlook.com>
2021-09-27 22:08:52 +0500

    add files

--- #source/js/components/_sliders.js
+++ #source/js/components/_sliders.js
@@ -1,7 +1,35 @@
 const firstSlider = document.querySelector('.first-slider'),
+  arrivalSlider = document.querySelector('.arrival-slider'),
   teamSlider = document.querySelector('.team-slider');
 
 const First = new Swiper(firstSlider, {
+  autoplay: {
+    delay: 3000,
+  },
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 35,
+  centerSlides: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.3,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    }
+  },
+  autoHeight: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.first-slider-next',
+    prevEl: '.first-slider-prev',
+  },
+});
+
+const Arrival = new Swiper( arrivalSlider, {
   autoplay: {
     delay: 3000,
   },
@@ -20,8 +48,8 @@ const First = new Swiper(firstSlider, {
   autoHeight: true,
   grabCursor: true,
   navigation: {
-    nextEl: '.first-slider-next',
-    prevEl: '.first-slider-prev',
+    nextEl: '.arrival-slider-next',
+    prevEl: '.arrival-slider-prev',
   },
 });
 
